239d5b5b23dfdc6ee048ed4cc16896cb1e9fc7c5
z.badar <z.badar@micromerger.com>
2019-10-31 12:00:34 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+To Run the project
+
+### `npm i`
+
+### `npm run dev` 
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/home/home';
+
+function App() {
+  return <Home name="React" />;
+}
+
+export default App;
